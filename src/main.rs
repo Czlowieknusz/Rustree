50506fae8fef1271305f8192282443e0c1a50f49
czlowieknusz <jnwznk@gmail.com>
2022-04-30 23:50:21 +0200

    Box<Option> changed to Option<Box>.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::cmp;
 #[derive(Debug)]
 struct Node {
     pub value: i32,
-    left_child: Box<Option<Node>>,
-    right_child: Box<Option<Node>>,
+    left_child: Option<Box<Node>>,
+    right_child: Option<Box<Node>>,
 }
 
 impl Node {
@@ -21,25 +21,35 @@ impl Node {
     }
 
     fn add_node(&mut self, value: i32) {
-        // match (
-        //     self.left_child.borrow_mut().as_ref(),
-        //     self.right_child.borrow_mut().as_ref(),
-        // ) {
-        //     (Some(mut left), Some(_)) => left.add_node(value),
-            /*Rc::get_mut(left).unwrap().add_node(value)*/
-            //
-            // )left.clone().add_node(value),
-            // (None, None) => self.left_child.borrow_mut().as_deref_mut() = Option::from(Box::new(Node::new(value))),
-            // (Some(_), _) => self.right_child.borrow_mut().as_deref_mut() = Option::from(Box::new(Node::new(value))),
-            // _ => {}
+        // if self.value == value {
+        //     println!("Value already present!");
+        //     return;
+        // }
+        // // match (&mut self.left_child, &mut self.right_child) {
+        // //     (Some(left), Some(right)) => {
+        // //
+        // //     },
+        // //     (None, _) =>,
+        // //     (Some(_), None) =>,
+        // // }
+        // let parent_node = if value > self.value { &mut self.right_child} else {&mut self.left_child};
+        // match parent_node. {
+        //     Some (&mut node) => node.
+        // //     (Some(mut left), Some(_)) => left.add_node(value),
+        //     /*Rc::get_mut(left).unwrap().add_node(value)*/
+        //     //
+        //     // )left.clone().add_node(value),
+        //     // (None, None) => self.left_child.borrow_mut().as_deref_mut() = Option::from(Box::new(Node::new(value))),
+        //     // (Some(_), _) => self.right_child.borrow_mut().as_deref_mut() = Option::from(Box::new(Node::new(value))),
+        //     // _ => {}
         // }
     }
 
     fn new(value: i32)  -> Node {
         Node {
             value,
-            left_child: Box::new(None),
-            right_child: Box::new(None),
+            left_child: None,
+            right_child: None,
         }
     }
 }
@@ -47,8 +57,8 @@ impl Node {
 fn main() {
     let mut node = Node {
         value: 3,
-        left_child: Box::new(None),
-        right_child: Box::new(None),
+        left_child: None,
+        right_child: None,
     };
     println!("Before adding Node is {:?} and it's depth is {}.", node, node.get_depth());
     node.add_node(5);
@@ -63,8 +73,8 @@ mod tests {
     fn single_node_tree() {
         let parent = Box::new(Node {
             value: 1,
-            left_child: Box::new(None),
-            right_child: Box::new(None),
+            left_child: None,
+            right_child: None,
         });
 
         assert_eq!(parent.get_depth(), 1);
@@ -74,8 +84,8 @@ mod tests {
     fn should_choose_larger_branch() {
         let mut parent = Box::new(Node {
             value: 5,
-            left_child: Box::new(None),
-            right_child: Box::new(None),
+            left_child: None,
+            right_child: None,
         });
         parent.add_node(3);
         parent.add_node(2);
@@ -89,8 +99,8 @@ mod tests {
     fn should_not_use_first_node_depth() {
         let mut parent = Box::new(Node {
             value: 3,
-            left_child: Box::new(None),
-            right_child: Box::new(None),
+            left_child: None,
+            right_child: None,
         });
         parent.add_node(5);
         parent.add_node(4);
@@ -104,8 +114,8 @@ mod tests {
     // fn add_left_then_right_node() {
     //     let mut parent = Node {
     //         value: 1,
-    //         left_child: Box::new(None),
-    //         right_child: Box::new(None),
+    //         left_child: None,
+    //         right_child: None,
     //     };
     //
     //     parent.add_node(3);
