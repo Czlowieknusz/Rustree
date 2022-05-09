abd1cb16b99cbbb2f4cd59970a4e0bb81ba888c8
czlowieknusz <jnwznk@gmail.com>
2022-05-09 19:24:08 +0200

    Deleted old empty file and usage of Counter.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,5 @@ use iced::{Application, Settings};
 mod iced_tree;
 
 fn main() -> Result<(), iced::Error> {
-    // iced_tree::Counter::run(Settings::default())
     iced_tree::Tree::run(Settings::default())
 }
