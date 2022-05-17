12cf3da02f41b18224d83a07cb25441c583ef2ca
czlowieknusz <jnwznk@gmail.com>
2022-05-17 21:53:24 +0200

    Compiles, tests pass but it crashes on run.

--- src/iced_tree/mod.rs
+++ src/iced_tree/mod.rs
@@ -5,7 +5,7 @@ pub mod node;
 
 #[derive(Default)]
 pub struct Tree {
-    root: Box<node::Node>,
+    root: Box<node::Tree>,
     add_child_btn: button::State,
     del_child_btn: button::State,
 }
@@ -65,12 +65,12 @@ impl Sandbox for Tree {
                     .align_items(Alignment::Center)
                     .push(Text::new(curr_depth).size(50)),
             )
-            .push(print_tree(&self.root))
+            .push(print_tree(self.root.as_ref()))
             .into()
     }
 }
 
-fn print_tree(node: &node::Node) -> Column<Message> {
+fn print_tree(node: &node::Tree) -> Column<Message> {
     let mut nodes = vec![Some(node)];
     let mut ret: Column<Message> = Column::new().push(Row::new().padding(15));
     let mut depth = node.get_depth();
@@ -84,7 +84,7 @@ fn print_tree(node: &node::Node) -> Column<Message> {
                 Some(node) => {
                     tree_layer = tree_layer.push(
                         Column::new()
-                            .push(Text::new(node.value.to_string()).size(25))
+                            .push(Text::new(node.0.as_ref().unwrap().value.to_string()).size(25))
                             .padding(padding)
                             .align_items(Alignment::Center),
                     )
@@ -117,17 +117,17 @@ fn calc_padding(depth: &u32) -> u16 {
     paddings.last().copied().unwrap()
 }
 
-fn get_next_iter_nodes(nodes: Vec<Option<&node::Node>>) -> Vec<Option<&node::Node>> {
-    let mut tmp_nodes: Vec<Option<&node::Node>> = vec![];
+fn get_next_iter_nodes(nodes: Vec<Option<&node::Tree>>) -> Vec<Option<&node::Tree>> {
+    let mut tmp_nodes: Vec<Option<&node::Tree>> = vec![];
     for n in nodes.iter() {
         match n {
             Some(n) => {
-                match n.left.0.as_ref() {
-                    Some(n) => tmp_nodes.push(Some(n)),
+                match n.0.as_ref().unwrap().left.0 {
+                    Some(_) => tmp_nodes.push(Some(&n.0.as_ref().unwrap().left)),
                     None => tmp_nodes.push(None),
                 };
-                match n.right.0.as_ref() {
-                    Some(n) => tmp_nodes.push(Some(n)),
+                match n.0.as_ref().unwrap().right.0 {
+                    Some(_) => tmp_nodes.push(Some(&n.0.as_ref().unwrap().right)),
                     None => tmp_nodes.push(None),
                 };
             }
@@ -140,7 +140,7 @@ fn get_next_iter_nodes(nodes: Vec<Option<&node::Node>>) -> Vec<Option<&node::Nod
     tmp_nodes
 }
 
-fn is_some_in_vec(v: &[Option<&node::Node>]) -> bool {
+fn is_some_in_vec(v: &[Option<&node::Tree>]) -> bool {
     v.iter().any(|&n| n.is_some())
 }
 
@@ -168,7 +168,7 @@ mod tests {
 
     #[test]
     fn one_some() {
-        let n = node::Node::new(3);
+        let n = node::Tree::new(3);
         let v = vec![None, None, Some(&n)];
         assert_eq!(is_some_in_vec(&v), true);
     }
@@ -182,8 +182,8 @@ mod tests {
 
     #[test]
     fn all_some() {
-        let n1 = node::Node::new(2);
-        let n2 = node::Node::new(3);
+        let n1 = node::Tree::new(2);
+        let n2 = node::Tree::new(3);
         let v = vec![Some(&n1), Some(&n2)];
 
         assert_eq!(is_some_in_vec(&v), true);
@@ -191,14 +191,14 @@ mod tests {
 
     #[test]
     fn should_add_some() {
-        let mut node = node::Node::new(2);
+        let mut node = node::Tree::new(2);
         node.add_node(1);
 
         let mut nodes = vec![Option::from(&node)];
 
         nodes = get_next_iter_nodes(nodes);
 
-        let exp_node = node::Node::new(1);
+        let exp_node = node::Tree::new(1);
         let expected_nodes = vec![Option::from(&exp_node), None];
         assert_eq!(nodes, expected_nodes);
     }
--- src/iced_tree/node.rs
+++ src/iced_tree/node.rs
@@ -1,7 +1,5 @@
 use std::cmp;
 
-use queues::*;
-
 #[derive(Debug, Default, PartialEq)]
 pub struct Node {
     pub value: i32,
@@ -11,13 +9,6 @@ pub struct Node {
 
 #[derive(Debug, Default, PartialEq)]
 pub struct Tree(pub Option<Box<Node>>);
-
-#[derive(Debug, Clone, Copy, PartialEq)]
-enum Successor<'a> {
-    LeftNode(&'a Node),
-    RightNode(&'a Node),
-    None,
-}
 
 impl Node {
     pub fn new(value: i32) -> Node {
@@ -52,22 +43,22 @@ impl Tree {
         }
 
         match (
-            self.0.unwrap().as_ref().left.0.as_ref(),
-            self.0.unwrap().as_ref().right.0.as_ref(),
+            self.0.as_ref().unwrap().left.0.as_ref(),
+            self.0.as_ref().unwrap().right.0.as_ref(),
         ) {
             (None, None) => 1,
-            (Some(_), None) => self.0.unwrap().as_ref().left.get_depth() + 1,
-            (None, Some(_)) => self.0.unwrap().as_ref().right.get_depth() + 1,
+            (Some(_), None) => self.0.as_ref().unwrap().left.get_depth() + 1,
+            (None, Some(_)) => self.0.as_ref().unwrap().right.get_depth() + 1,
             (Some(_), Some(_)) => {
                 cmp::max(
-                    self.0.unwrap().as_ref().left.get_depth() + 1,
-                    self.0.unwrap().as_ref().right.get_depth() + 1,
+                    self.0.as_ref().unwrap().left.get_depth(),
+                    self.0.as_ref().unwrap().right.get_depth(),
                 ) + 1
             }
         }
     }
 
-    fn new(val: i32) -> Tree {
+    pub fn new(val: i32) -> Tree {
         Tree(Some(Box::new(Node::new(val))))
     }
 
@@ -86,11 +77,9 @@ impl Tree {
                             return true;
                         }
                     }
-                }
-                if node.value > value {
+                } else if node.value > value {
                     current = &mut node.left;
-                }
-                if node.value < value {
+                } else if node.value < value {
                     current = &mut node.right;
                 }
             }
@@ -120,14 +109,14 @@ mod tests {
 
     #[test]
     fn single_node_tree() {
-        let root = Box::new(Node::new(1));
+        let root = Tree::new(1);
 
         assert_eq!(root.get_depth(), 1);
     }
 
     #[test]
     fn left_depth_if_bigger() {
-        let mut root = Box::new(Node::new(5));
+        let mut root = Tree::new(5);
         root.add_node(3);
         root.add_node(2);
         root.add_node(1);
@@ -138,7 +127,7 @@ mod tests {
 
     #[test]
     fn right_depth_if_bigger() {
-        let mut root = Box::new(Node::new(3));
+        let mut root = Tree::new(3);
         root.add_node(5);
         root.add_node(4);
         root.add_node(6);
@@ -149,7 +138,7 @@ mod tests {
 
     #[test]
     fn depth_is_one_hundred() {
-        let mut root = Box::new(Node::new(0));
+        let mut root = Tree::new(0);
         for i in 1..100 {
             root.add_node(i);
         }
@@ -170,7 +159,7 @@ mod tests {
 
     #[test]
     fn should_delete_root() {
-        let mut root = Box::new(Node::new(0));
+        let mut root = Tree::new(0);
         assert_eq!(root.get_depth(), 1);
 
         root.del_node(0);
@@ -178,72 +167,24 @@ mod tests {
     }
 
     #[test]
-    fn find_value_in_single_element_tree() {
-        let root = Box::new(Node::new(3));
-        assert_eq!(root.find_node(3).unwrap(), root.as_ref());
-    }
-
-    #[test]
-    fn find_value_in_multi_elements_tree() {
-        let mut root = Box::new(Node::new(5));
-        for i in 0..9 {
-            root.add_node(i);
-        }
-        assert_eq!(root.get_depth(), 6);
-        assert_eq!(
-            root.find_node(6).unwrap(),
-            root.right.0.as_ref().unwrap().as_ref()
-        );
-    }
-
-    #[test]
-    fn return_none_if_value_not_present() {
-        let mut root = Box::new(Node::new(5));
-        for i in 0..9 {
-            root.add_node(i);
-        }
-        assert_eq!(root.find_node(12), None);
-    }
-
-    #[test]
-    fn find_none_in_single_node_tree() {
-        let root = Box::new(Node::new(5));
-        assert_eq!(root.find_successor_parent(), Successor::None);
-    }
-
-    #[test]
-    fn find_successor_parent_two_subtrees() {
-        let root = prepare_tree();
-        if let Successor::LeftNode(succ_parent) = root.find_successor_parent() {
-            assert_eq!(succ_parent.value, 7);
-        } else {
-            panic!("Wrong node chosen as successor!");
+    fn extract_node_from_complex_tree() {
+        let mut root = prepare_tree();
+        unsafe {
+            assert_eq!(root.0.as_mut().unwrap().right.extract_min(), 6);
         }
     }
 
     #[test]
-    fn find_successor_parent_with_only_left_subtree() {
-        let mut root = Box::new(Node::new(5));
+    fn extract_left_node() {
+        let mut root = Tree::new(5);
         root.add_node(3);
-        if let Successor::LeftNode(succ_parent) = root.find_successor_parent() {
-            assert_eq!(succ_parent.value, 5);
-        } else {
-            panic!("Wrong node chosen as successor!");
+
+        unsafe {
+            assert_eq!(root.extract_min(), 3);
         }
     }
 
-    #[test]
-    fn right_node_has_no_left_child() {
-        let mut root = Box::new(Node::new(5));
-        root.add_node(6);
-        if let Successor::RightNode(succ_parent) = root.find_successor_parent() {
-            assert_eq!(succ_parent.value, 5);
-        } else {
-            panic!("Wrong node chosen as successor!");
-        }
-    }
-
-    fn prepare_tree() -> Box<Node> {
+    fn prepare_tree() -> Tree {
         /*
                 5
              /     \
@@ -253,7 +194,7 @@ mod tests {
         \   /           \
          1 3             9
         */
-        let mut root = Box::new(Node::new(5));
+        let mut root = Tree::new(5);
         // left
         root.add_node(2);
         root.add_node(0);
